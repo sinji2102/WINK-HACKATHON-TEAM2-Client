ea1beb5a58ae5ec7c1a5c5ba0d647bcffa27465a
Sinji <sinzi031012@naver.com>
2024-08-30 23:39:29 +0900

    init: 모바일뷰 초기세팅 및 라우터 세팅

---
+++ src/routes/Router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  //   {
+  //     path: "/A",
+  //     element: <APage />,
+  // export const router = createBrowserRouter([
+  //   },
+  //   ...
+]);
+export default router;
