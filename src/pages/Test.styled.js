d06e7bf07f0900dfc93badda091524481734c3df
Sinji <sinzi031012@naver.com>
2024-08-31 02:43:45 +0900

    init: 색상 초기세팅

--- src/pages/Test.styled.js
+++ src/pages/Test.styled.js
@@ -3,4 +3,5 @@ import styled from "styled-components";
 export const TestWrapper = styled.section`
   width: auto;
   ${({ theme }) => theme.fonts.heading2};
+  background-color: ${({ theme }) => theme.colors.b01};
 `;
