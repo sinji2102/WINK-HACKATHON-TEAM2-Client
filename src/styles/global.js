03f83c752ea35edb54d14ed8d92142c97d430715
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2024-08-31 00:18:41 +0900

    style: 모바일 뷰 세팅 반응형으로 변경

--- src/styles/global.js
+++ src/styles/global.js
@@ -5,47 +5,64 @@ const global = createGlobalStyle`
     ${reset}
     
     * {
-    box-sizing: border-box;
+      box-sizing: border-box;
     }
+    
     html {
-    font-size: 62.5%;
-    background-color: black;
-    user-select: none;
-    -webkit-touch-callout: none;
-    -webkit-tap-highlight-color:rgb(0 0 0 / 0%);
-    scroll-behavior: smooth; 
+      font-size: 62.5%;
+      background-color: black;
+      user-select: none;
+      -webkit-touch-callout: none;
+      -webkit-tap-highlight-color:rgb(0 0 0 / 0%);
+      scroll-behavior: smooth; 
     }
+    
     ul, li {
-    padding-left: 0;
-    list-style: none;
+      padding-left: 0;
+      list-style: none;
     }
+    
     a {
-    color: inherit;
-    text-decoration: none;
+      color: inherit;
+      text-decoration: none;
     }
+    
     input, button {
-    background-color: transparent;
-    outline: none;
-    border: none;
+      background-color: transparent;
+      outline: none;
+      border: none;
     }
+    
     button {
-    padding: 0;
-    cursor: pointer;
+      padding: 0;
+      cursor: pointer;
     }
+    
     input {
-    appearance: none;
-    &:focus {
+      appearance: none; 
+      &:focus {
         outline: none;
+      }
     }
-    }
-
-    /* 모바일뷰 세팅 */
-    body {
-        width: 375px; 
+    
+    @media screen and (max-width: 375px) {  
+      body {
+        width: 100%;
         min-height: 100%;
-        margin: 0 auto; 
+        margin: 0 auto;
         overflow-x: hidden;
-        background-color: gray ;
+        background-color: gray;
+      }
+    }
+    
+    @media screen and (min-width: 376px) {
+      body {
+        width: 375px;
+        min-height: 100%;
+        margin: 0 auto;
+        overflow-x: hidden;
+        background-color: gray;
+      }
     }
 `;
 
