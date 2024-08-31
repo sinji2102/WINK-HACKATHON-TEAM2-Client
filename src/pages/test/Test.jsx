0e94d17280553ef88049962cee7f784449714a7a
Sinji <sinzi031012@naver.com>
2024-08-31 21:45:35 +0900

    feat: TextField 기능 구현

--- src/pages/test/Test.jsx
+++ src/pages/test/Test.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import { TestWrapper } from "./Test.styled";
 import TextArea from "../../components/commons/input/textArea/TextArea";
+import TextField from "../../components/commons/input/textField/TextField";
 
 const Test = () => {
   const [inputAreaValue, setInputAreaValue] = useState("");
+  const [inputFieldValue, setInputFieldValue] = useState("");
 
   const handleChangeInputArea = (e) => {
     setInputAreaValue(e.target.value);
+  };
+
+  const handleChangeInputField = (e) => {
+    setInputFieldValue(e.target.value);
   };
 
   return (
@@ -16,7 +22,14 @@ const Test = () => {
         value={inputAreaValue}
         onChange={handleChangeInputArea}
         maxLength={300}
-      ></TextArea>
+      />
+      <TextField
+        value={inputFieldValue}
+        onChange={handleChangeInputField}
+        placeholder="테스트입니당"
+        name="test"
+        type="input"
+      />
     </TestWrapper>
   );
 };
