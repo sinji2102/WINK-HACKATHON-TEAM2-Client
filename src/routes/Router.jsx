9fdc0f7cac609fc47f79b6d8cd7e8978a8169d48
Sinji <sinzi031012@naver.com>
2024-08-31 22:48:57 +0900

    chore: 잘못된 경로 수정

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Test from "../pages/test/Test.jsx";
+import Register from "../pages/register/Register.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/test",
     element: <Test />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
 ]);
 export default router;
